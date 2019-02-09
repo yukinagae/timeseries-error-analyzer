03fe204f72fde7150a0858ce3c6a1858582d7911
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-02-09 13:26:11 +0900

    sample code

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from distutils.core import setup
 
+REQUIREMENTS = [
+    "numpy",
+    "sklearn",
+]
+
 setup(
     name='ts-error',
     version='0.1dev',
+    author='Yuki Nagae',
+    author_email='yuki.nagae1130@gmail.com',
+    install_requires=REQUIREMENTS,
     packages=['ts-error'],
     license='MIT',
+    description='A tiny python package analyzes time series prediction errors',
     long_description=open('README.md').read(),
 )
---
+++ ts-error/metrics/regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+__ALL__ = [
+    "root_mean_squared_error"
+]
+
+
+def root_mean_squared_error(y_true, y_pred):
+  return np.sqrt(mean_squared_error(y_pred, y_true))
