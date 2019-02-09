e5fffb218ea1036cb7b967d3e965e80802f7367d
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-02-09 12:54:12 +0900

    Specify dev version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ts-error',
-    version='0.1.0',
+    version='0.1dev',
     packages=['ts-error'],
     license='MIT',
     long_description=open('README.md').read(),
